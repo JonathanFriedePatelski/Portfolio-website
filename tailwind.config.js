b319bd9bab029dae1e512d308f5790dfc7a5a6e0
jonathan patelski <Jonathanpatelski@gmail.com>
2023-11-09 16:07:04 +0100

    feat: added tempdrawing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./resources/views/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Playpen Sans', 'Inter']
+      },
+      colors: {
+        primary: '#14b8a6',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
