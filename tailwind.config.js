d6f1d188788577123f6a1c0452ac9ea5b090233b
jonathan patelski <Jonathanpatelski@gmail.com>
2023-11-15 15:03:37 +0100

    feat: finalized hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/views/*.html'],
+  content: ["./resources/views/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        body: ['Playpen Sans', 'Inter']
+        body: ["Playpen Sans", "Inter"],
       },
       colors: {
-        primary: '#14b8a6',
+        primary: "#14b8a6",
         secondary: {
-          100: '#E2E2D5',
-          200: '#888883',
-        }
-      }
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
